86293190c0b544a6c2d1cbcb62dac6ff2a66a0b4
andrea <andrealiureta@gmail.com>
2021-11-17 21:23:26 +0400

    Make clean template for tutorial

--- app/src/main/java/com/andreasgift/ikigai/Expense.kt
+++
@@ -1,8 +0,0 @@
-package com.andreasgift.ikigai
-
-enum class expenseCategory { Groceries, Transportation, Food, Misc }
-class Expense(
-    val category: expenseCategory,
-    val wallet: String,
-    val amount: Int
-)
